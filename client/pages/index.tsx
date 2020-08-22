dacb82cc9d4f4c9b508e0c4407079e40958ad24a
John Clark <clark@itsmechlark.com>
2020-08-22 12:56:55 +0000

    Remove Features  on Index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,7 +7,6 @@ import Extensions from "../components/Extensions";
 import LinksTable from "../components/LinksTable";
 import AppWrapper from "../components/AppWrapper";
 import Shortener from "../components/Shortener";
-import Features from "../components/Features";
 import Footer from "../components/Footer";
 import { useStoreState } from "../store";
 
@@ -28,7 +27,6 @@ const Homepage = () => {
       <Shortener />
       {!isAuthenticated && <NeedToLogin />}
       {isAuthenticated && <LinksTable />}
-      <Features />
       <Extensions />
       <Footer />
     </AppWrapper>
