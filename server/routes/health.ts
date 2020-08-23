83a284f9a0863527cfd12989e07486cc7b57f3ab
John Clark <clark@itsmechlark.com>
2020-08-23 17:44:28 +0000

    Added health check for K8s

--- server/routes/health.ts
+++ server/routes/health.ts
@@ -2,6 +2,19 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (_, res) => res.send("OK"));
+router.get("/", (_, res) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now()
+  };
+
+  try {
+    res.send(healthcheck);
+  } catch (e) {
+    healthcheck.message = e;
+    res.status(503).send(healthcheck);
+  }
+});
 
 export default router;
