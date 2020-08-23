2a21b7546a00c6113a477c3142b371cd2b373d45
John Clark <clark@itsmechlark.com>
2020-08-23 15:24:04 +0000

    Use process env for Next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
-const { parsed: localEnv } = require("dotenv").config();
-
 module.exports = {
   publicRuntimeConfig: {
-    CONTACT_EMAIL: localEnv && localEnv.CONTACT_EMAIL,
-    SITE_NAME: localEnv && localEnv.SITE_NAME,
-    DEFAULT_DOMAIN: localEnv && localEnv.DEFAULT_DOMAIN,
-    RECAPTCHA_SITE_KEY: localEnv && localEnv.RECAPTCHA_SITE_KEY,
-    GOOGLE_ANALYTICS: localEnv && localEnv.GOOGLE_ANALYTICS,
-    REPORT_EMAIL: localEnv && localEnv.REPORT_EMAIL,
-    DISALLOW_ANONYMOUS_LINKS: localEnv && localEnv.DISALLOW_ANONYMOUS_LINKS,
-    DISALLOW_REGISTRATION: localEnv && localEnv.DISALLOW_REGISTRATION,
-    SENTRY_PUBLIC_DSN: localEnv && localEnv.SENTRY_PUBLIC_DSN,
-    DISALLOW_DOMAIN: localEnv && localEnv.DISALLOW_DOMAIN,
+    CONTACT_EMAIL: process.env.CONTACT_EMAIL,
+    SITE_NAME: process.env.SITE_NAME,
+    DEFAULT_DOMAIN: process.env.DEFAULT_DOMAIN,
+    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
+    GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
+    REPORT_EMAIL: process.env.REPORT_EMAIL,
+    DISALLOW_ANONYMOUS_LINKS: process.env.DISALLOW_ANONYMOUS_LINKS,
+    DISALLOW_REGISTRATION: process.env.DISALLOW_REGISTRATION,
+    SENTRY_PUBLIC_DSN: process.env.SENTRY_PUBLIC_DSN,
+    DISALLOW_DOMAIN: process.env.DISALLOW_DOMAIN,
   }
 };
